bf345f1aef742d37acf1e874975bb8a492c786c3
Poonam Malve <poonam.malve@gowitek.com>
2019-11-08 18:51:14 +0530

    testing the first app

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Starting...")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world..")
+	})
+
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
